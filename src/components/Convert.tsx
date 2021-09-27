0214391bb88946b0334f685cc3cc9fca04d65754
kanako-110 <kanako.110.ito@gmail.com>
2021-09-27 22:37:10 +0900

    :art: add
    textfield

---
+++ src/components/Convert.tsx
@@ -0,0 +1,29 @@
+import { useState } from 'react';
+import CurrencyTextField from '@unicef/material-ui-currency-textfield';
+
+// const countryOptions = [
+// 	{ value: 'BND', text: 'BND' },
+// 	{ value: 'USD', text: 'USD' },
+// 	{ value: 'CAD', text: 'CAD' },
+// ];
+
+export const Convert = () => {
+	const [amount, setAmount] = useState(0);
+
+	return (
+		<div>
+			<CurrencyTextField
+				label="USD"
+				value={amount}
+				currencySymbol="$"
+				digitGroupSeparator=","
+				onChange={(_, value: number) => setAmount(value)}
+			/>
+			<CurrencyTextField
+				label="JPY"
+				currencySymbol="￥"
+				digitGroupSeparator=","
+			/>
+		</div>
+	);
+};
